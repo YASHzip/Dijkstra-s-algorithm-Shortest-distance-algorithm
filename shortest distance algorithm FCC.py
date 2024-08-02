13f0bf9f398f8594dfdc86e020cff45ab8ed69ba
Yashvardhan Singh <singhyashvardhan402@gmail.com>
2024-08-02 22:26:42 +0530

    Update shortest distance algorithm FCC.py

--- shortest distance algorithm FCC.py
+++ shortest distance algorithm FCC.py
@@ -9,7 +9,7 @@ my_graph = {
 
 def shortest_path(graph, start, target = ''):
     unvisited = list(graph)
-    distances = {node: 0 if node == start else float('-inf') for node in graph}
+    distances = {node: 0 if node == start else float('inf') for node in graph}
     paths = {node: [] for node in graph}
     paths[start].append(start)
 
